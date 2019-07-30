bc8d4a2247fcbc71e94e737113c622b99e523339
murohi <52985297+murohi@users.noreply.github.com>
2019-07-30 13:45:08 +0900

    Update FFT.py

--- FFT.py
+++ FFT.py
@@ -1,12 +1,13 @@
+#Aに画像をグレースケールに変換していれる
 A = rgb2gray(imread('images/1-140105025213.jpg'))
 imshow(A)
 #plt.color()
 plt.show()
-
+#LgTransという関数を定義
 def LgTrans(x):
     C = 1/log(1+1)
     return C*log(1+x)
-
+#フーリエ変換をする
 im = np.abs(np.fft.fft2(A))
 
 h,w = im.shape
@@ -23,14 +24,3 @@ plt.show()
 
 print("max: ",im.max())
 print("min: ",im.min())
-
-im2 = np.abs(np.fft.ifft(im))
-
-
-
-imshow(im2, vmin=0)
-plt.colorbar()
-plt.show()
-
-print("max: ",im2.max())
-print("min: ",im2.min())
